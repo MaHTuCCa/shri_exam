f952973cb347966e014bfa32642f188372e0c9c5
MaHTuCCa <akimovamp@yandex.ru>
2013-10-14 02:23:50 +0400

    add lections/lectors template

--- js/template-load.js
+++ js/template-load.js
@@ -4,21 +4,51 @@ $.getJSON('data/data.json', function(data) {
     var template = $('#menu_template').html();
     var html = Mustache.to_html(template, data);
     $('#menu').html(html);
+
     loadData();
 });
 
+$(window).on("popstate", function(e) {
+   loadData();
+});
+
+function goToURL(url) {
+    history.pushState(null, null, url);
+    loadData();
+}
+
 function loadData() {
     var params = new Object;
-    var tt = document.location.href.split("?")[1].split('&');
-    for(i in tt) {
-        params[tt[i].split("=")[0]] = tt[i].split("=")[1];
+    var paramPair = document.location.href.split("?")[1];
+    if(paramPair == null) {
+        params['page'] = 'home';
+    } else {
+        paramPair = paramPair.split("#")[0].split('&');
+        for(i in paramPair) {
+            params[paramPair[i].split("=")[0]] = paramPair[i].split("=")[1];
+        }
     }
+
     for(i in json_data.pages) {
         var page = json_data.pages[i];
         if(page.name == params['page']) {
-            var template = $('#'+page.name+'_template').html();
-            if(page.data != null)
-                template = Mustache.to_html(template, page.data);
+            var tmpl_name = page.name;
+            var data = page.data;
+            if(params['item'] >= 0) {
+                tmpl_name += '_item';
+                data = null;
+                for(_data in page.data) {
+                    for(i in page.data[_data]) {
+                        if(page.data[_data][i].id == params['item']) {
+                            data = page.data[_data][i];
+                        }
+                    }
+                }
+            }
+            var template = $('#'+tmpl_name+'_template').html();
+
+            if(data != null)
+                template = Mustache.to_html(template, data);
             $("#content").html(template);
         }
     }
