3036b1ee93da876c0b28aa584010b54091588bdc
MaHTuCCa <akimovamp@yandex.ru>
2013-10-13 21:14:35 +0400

    make single page application

--- js/template-load.js
+++ js/template-load.js
@@ -1,14 +1,26 @@
-
-$.getJSON('data/student_list.json', function(data) {
-    var template = $('#student_template').html();
+var json_data;
+$.getJSON('data/data.json', function(data) {
+    json_data = data;
+    var template = $('#menu_template').html();
     var html = Mustache.to_html(template, data);
-    html = html.split("&lt;").join("<").split("&gt;").join(">").split("&#x2F;").join("/");
-    $('#student_template').html(html);
+    $('#menu').html(html);
+    loadData();
 });
 
-$.getJSON('data/lection_list.json', function(data) {
-    var template = $('#lection_template').html();
-    var html = Mustache.to_html(template, data);
-    html = html.split("&lt;").join("<").split("&gt;").join(">").split("&#x2F;").join("/");
-    $('#lection_template').html(html);
-});
+function loadData() {
+    var params = new Object;
+    var tt = document.location.href.split("?")[1].split('&');
+    for(i in tt) {
+        params[tt[i].split("=")[0]] = tt[i].split("=")[1];
+    }
+    for(i in json_data.pages) {
+        var page = json_data.pages[i];
+        if(page.name == params['page']) {
+            var template = $('#'+page.name+'_template').html();
+            if(page.data != null)
+                template = Mustache.to_html(template, page.data);
+            $("#content").html(template);
+        }
+    }
+}
+
